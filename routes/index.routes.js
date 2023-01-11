b466cb3790afce9ed888979bd0c4b9638bad8ea2
Emmanuel <emmanuelugwueze6@gmail.com>
2023-01-11 23:09:22 +0100

    user routes2

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import postRouter from './post.router.js';
 const indexRouter = express.Router();
+import userRoute from './user.router.js';
 
 indexRouter.use('/post', postRouter)
+indexRouter.use('/user', userRoute);
 
 export default indexRouter;
