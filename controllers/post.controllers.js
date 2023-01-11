8236d1baee3d3d79012c4bac200a6b3e99acef7e
Emmanuel <emmanuelugwueze6@gmail.com>
2023-01-11 08:30:27 +0100

    feat:find post by id

--- controllers/post.controllers.js
+++ controllers/post.controllers.js
@@ -10,6 +10,15 @@ class PostControllers{
         })
         
     }
+    async findPostById(req, res) {
+        const findPost = Post.findOne({ where: { id: req.params.id } });
+        findPost.then(val => {
+            return res.status(200).send({ status: true, data: val });
+        }).catch(err=>{
+            return res.status(500).send({ status: false, message: err });
+        })
+        
+    }
 }
 
 export default new PostControllers();
--- routes/post.router.js
+++ routes/post.router.js
@@ -4,6 +4,7 @@ import postControllers from '../controllers/post.controllers.js';
 const postRouter = express.Router();
 
 postRouter.post('/', postControllers.createNewPost);
+postRouter.post('/:id', postControllers.findPostById);
 
 
 export default postRouter;
