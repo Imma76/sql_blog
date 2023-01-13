558b77636f7c735277066c28569a6c7e4e7cda20
Emmanuel <emmanuelugwueze6@gmail.com>
2023-01-13 14:18:57 +0100

    added validator to post routes

--- routes/comment.routes.js
+++ routes/comment.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import { body } from 'express-validator';
 import commentController from '../controllers/comment.controller.js';
 const commentRoutes = express.Router();
 
 
-commentRoutes.post('/',commentController.postComment);
+commentRoutes.post('/',[body('comment').isString().withMessage('please pass in a comment'), body('postId').isInt().withMessage('post is missing')],commentController.postComment);
 commentRoutes.get('/:id', commentController.getCommentbyId);
 commentRoutes.delete('/:id',commentController.deleteCommentById);
 
