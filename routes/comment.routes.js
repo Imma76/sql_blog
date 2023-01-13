828ad5c8819b8e23d98994e64fb464bdfc815b1f
Emmanuel <emmanuelugwueze6@gmail.com>
2023-01-13 14:21:59 +0100

    feat:added validator to get comment routes && delete ccomment

--- routes/comment.routes.js
+++ routes/comment.routes.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import { body } from 'express-validator';
+import { body,param } from 'express-validator';
 import commentController from '../controllers/comment.controller.js';
 const commentRoutes = express.Router();
 
 
 commentRoutes.post('/',[body('comment').isString().withMessage('please pass in a comment'), body('postId').isInt().withMessage('post is missing')],commentController.postComment);
-commentRoutes.get('/:id', commentController.getCommentbyId);
-commentRoutes.delete('/:id',commentController.deleteCommentById);
+commentRoutes.get('/:id',[param('id').isInt().withMessage('pass in comment id')], commentController.getCommentbyId);
+commentRoutes.delete('/:id',[param('id').isInt().withMessage('pass is comment id')],commentController.deleteCommentById);
 
 
 export default commentRoutes;
