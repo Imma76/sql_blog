a670e9c44b814c62c3be9925edd8c2aa8997f68f
Emmanuel <emmanuelugwueze6@gmail.com>
2023-01-13 13:58:52 +0100

    feat:create and delete comment

--- controllers/comment.controller.js
+++ controllers/comment.controller.js
@@ -4,11 +4,25 @@ class CommentController{
     
     
     async postComment(req, res) {
-               
+        const create = Comment.create({ comment: req.body.comment, postId: req.body.postId }).then(val => { return res.status(201).json({ status: true, message: 'comemnt posted successfully' }) }).catch((err)=>{
+            return res.status(500).json({status:false, message:`${err}`})
+        });
+        
     }
 
 
-    async deleteComment(req, res) {
+    async deleteCommentById(req, res) {
+        const deleteComment = Comment.findOne({ where: { id: req.params.postId } }).then(val => {
+            val.destroy().then(val => {
+                 return res.status(200).json({status:true, message:'comment deleted sucessfully'})
+            }).catch((err)=>{
+                return res.status(500).json({status:false, message:`${err}`})
+            });
+            
+           
+        }).catch((err)=>{
+            return res.status(500).json({status:false, message:`${err}`})
+        });
         
     }
 
