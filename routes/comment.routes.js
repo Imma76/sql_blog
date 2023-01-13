767c9ef5fad501d81981216c862d7335e24c06b0
Emmanuel <emmanuelugwueze6@gmail.com>
2023-01-13 13:45:44 +0100

    comment routes

---
+++ routes/comment.routes.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const commentRoutes = express.Router();
+
+
+commentRoutes.post('/',);
+commentRoutes.get('/:id');
+commentRoutes.delete('/:id');
+
+
+export default commentRoutes;
