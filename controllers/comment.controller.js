89606f6473b441817a1553454f60945d61103c26
Emmanuel <emmanuelugwueze6@gmail.com>
2023-01-13 13:53:00 +0100

    initialized comment controller

---
+++ controllers/comment.controller.js
@@ -0,0 +1,24 @@
+import Comment from "../models/comment.model.js";
+
+class CommentController{
+    
+    
+    async postComment(req, res) {
+               
+    }
+
+
+    async deleteComment(req, res) {
+        
+    }
+
+    async getCommentbyId(req, res) {
+        
+    }
+
+    async getComments(req, res) {
+        
+    }
+
+
+}
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,8 +2,10 @@ import express from 'express';
 import postRouter from './post.router.js';
 const indexRouter = express.Router();
 import userRoute from './user.router.js';
+import commentRoutes from './comment.routes.js';
 
 indexRouter.use('/post', postRouter)
 indexRouter.use('/user', userRoute);
+indexRouter.use('/comment', commentRouter);
 
 export default indexRouter;
