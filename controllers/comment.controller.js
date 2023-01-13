4ab646ee45a0ff898b4c8c97518fa71bbcb730ea
Emmanuel <emmanuelugwueze6@gmail.com>
2023-01-13 21:26:49 +0100

    validator changes

--- controllers/comment.controller.js
+++ controllers/comment.controller.js
@@ -1,12 +1,12 @@
 import errorHandler from "../utils/validator.handler.js";
 import Comment from "../models/comment.model.js";
-import { validationResult } from "express-validator";
+
 
 class CommentController{
     
     
     async postComment(req, res) {
-        errorHandler(req);
+        errorHandler(req,res);
         const create = Comment.create({ comment: req.body.comment, postId: req.body.postId }).then(val => { return res.status(201).json({ status: true, message: 'comemnt posted successfully' }) }).catch((err)=>{
             return res.status(500).json({status:false, message:`${err}`})
         });
@@ -15,7 +15,7 @@ class CommentController{
 
 
     async deleteCommentById(req, res) {
-        errorHandler(req);
+        errorHandler(req,res);
         const deleteComment = Comment.findOne({ where: { id: req.params.id } }).then(val => {
             val.destroy().then(val => {
                  return res.status(200).json({status:true, message:'comment deleted sucessfully'})
@@ -31,7 +31,7 @@ class CommentController{
     }
 
     async getCommentbyId(req, res) {
-        errorHandler(req);
+        errorHandler(req,res);
         const comment = Comment.findOne({ where: { postId: req.params.id } }).then(val => {
             return res.status(200).json({status:true,data:val})
         }).catch((err)=>{
@@ -40,7 +40,7 @@ class CommentController{
     }
 
     async getComments(req, res) {
-        errorHandler(req);
+        errorHandler(req,res);
         const comment = Comment.findAll().then(val => {
             return res.status(200).json({status:true,data:val})
         }).catch((err)=>{
--- utils/validator.handler.js
+++ utils/validator.handler.js
@@ -1,6 +1,9 @@
-const errorHandler= (req)=> {
+import { validationResult } from "express-validator";
+
+
+const errorHandler = (req,res) => {
     const error = validationResult(req);
-  if(!error.array().isEmpty){
+  if(!error.isEmpty()){
       return res.status(500).json({ status: false, message: error.array()[0].nestedErrors });
   }
 }
